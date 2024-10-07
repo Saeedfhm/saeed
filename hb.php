a943929594c30f02b3f086a4bbe1427be8977a4a
unknown <saeedfahimi3614@gmail.com>
2024-10-07 11:57:23 +0330

    add hb.php

--- hb.php
+++ hb.php
@@ -1,18 +1,6 @@
 <?php
 
-function get_module(){
-  return "hello t world";
-}
-
-function return_hello(){
-  rteturn "hello world";
-}
-
-
-function return_no_hello(){
-  rteturn "hello no world";
-}
-
+echo "heelo to world";
 
 
  ?>
